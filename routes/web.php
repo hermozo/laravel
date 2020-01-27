385e8601e6a7aa6b7c3b0461346140f589049778
angel <miguel.huaycho@exxis-group.com>
2020-01-27 00:43:30 -0600

    route resource

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ Route::get('vista', function() {
     return view('vista');
 });
 
-Route::get('controlador', 'ControladorController@index');
+//Route::get('controlador', 'ControladorController@index');
 
 
 Route::get('redireccionar', function() {
     return Redirect::to('/');
 });
+
+Route::resource('controlador', 'ControladorController');
